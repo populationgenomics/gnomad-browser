5635382fd8bfe950805091fb39ef888ea3f12794
Milo Hyben <milo.hyben@gmail.com>
2025-02-28 18:11:48 +1100

    Updated Menu App Name.

--- browser/src/NavBar.tsx
+++ browser/src/NavBar.tsx
@@ -92,7 +92,7 @@ const NavBar = () => {
     <Wrapper>
       <LogoWrapper>
         <Link to="/" onClick={closeMenu}>
-          <Logo>gnomAD browser</Logo>
+          <Logo>OurDNA Browser</Logo>
         </Link>
         <ToggleMenuButton onClick={toggleMenu}>☰</ToggleMenuButton>
       </LogoWrapper>
