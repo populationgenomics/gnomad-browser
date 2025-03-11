745ce021bd0230a51be982c833d0bbf52b031fa4
Milo Hyben <milo.hyben@gmail.com>
2025-03-11 15:19:07 +1100

    Added extra checks into gnomad-v4-variant-queries so it won't crash if joint ot exome fields are not present.

--- graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
+++ graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
@@ -91,16 +91,16 @@ const fetchVariantById = async (esClient: any, variantId: any, subset: Subset) =
 
   const variant = response.body.hits.hits[0]._source.value
   
-  logger.info(`Variant found ${variant}`)
+  logger.info(`Variant found ${JSON.stringify(variant)}`)
 
   const subsetGenomeFreq = variant.genome.freq.all || {}
-  const subsetJointFreq = variant.joint.freq[subset] || {}
+  const subsetJointFreq = variant.joint?.freq[subset] || {}
 
-  const hasExomeVariant = variant.exome.freq[subset].ac_raw
+  const hasExomeVariant = variant.exome?.freq[subset].ac_raw
   const hasGenomeVariant = subsetGenomeFreq.ac_raw
   const hasJointFrequencyData = subsetJointFreq.ac_raw
 
-  if (!subsetGenomeFreq.ac_raw && !(variant.exome.freq[subset] || {}).ac_raw) {
+  if (!subsetGenomeFreq.ac_raw && !(variant.exome?.freq[subset] || {}).ac_raw) {
     throw new UserVisibleError('Variant not found in selected subset.')
   }
 
