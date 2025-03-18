f574b0a666e14ede3ed490be8f4d69aea3f8ee86
Milo Hyben <milo.hyben@gmail.com>
2025-03-19 09:40:38 +1100

    Fixing linting.

--- deploy/deployctl/subcommands/elasticsearch.py
+++ deploy/deployctl/subcommands/elasticsearch.py
@@ -6,7 +6,7 @@ import typing
 from deployctl.shell import kubectl
 
 
-def get_elasticsearch_password(cluster_name: str, namespace: str) -> None:
+def get_elasticsearch_password(_cluster_name: str, namespace: str) -> None:
     # ECK creates this secret when the cluster is created.
     print(
         kubectl(
@@ -21,14 +21,14 @@ def get_elasticsearch_password(cluster_name: str, namespace: str) -> None:
     )
 
 
-def load_datasets(cluster_name: str, namespace: str, dataproc_cluster: str, secret: str, datasets: str):
+def load_datasets(_cluster_name: str, namespace: str, dataproc_cluster: str, secret: str, datasets: str):
     # Matches service name in deploy/manifests/elasticsearch.load-balancer.yaml.jinja2
     elasticsearch_load_balancer_ip = kubectl(
         [
             f"-n={namespace}",
             "get",
             "service",
-            f"gnomad-elasticsearch-lb",
+            "gnomad-elasticsearch-lb",
             "--output=jsonpath={.status.loadBalancer.ingress[0].ip}",
         ]
     )
