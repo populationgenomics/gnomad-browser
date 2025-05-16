2260f9eaa827ef85721be0088299bcc0714de876
Amy Miniter <40307120+amyminiter@users.noreply.github.com>
2025-05-16 14:38:43 +1000

    Fix linting issue

--- browser/src/AboutPage.tsx
+++ browser/src/AboutPage.tsx
@@ -5,43 +5,12 @@ import { PageHeading } from '@gnomad/ui'
 // @ts-expect-error
 import aboutContent from '../about/about.md'
 // import contributingProjectsList from '../about/contributors/contributing-projects.md'
-// @ts-expect-error
-import fundingSources from '../about/contributors/funding.md'
 // import dataContributorsList from '../about/contributors/data-contributors.md'
 // import gcbrContent from '../about/contributors/gcbr/gcbr.md'
 
 import DocumentTitle from './DocumentTitle'
 import InfoPage from './InfoPage'
 import MarkdownContent from './MarkdownContent'
-
-const Credits = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  @media (max-width: 992px) {
-    flex-direction: column;
-    font-size: 16px;
-  }
-`
-
-const SectionHeader = styled.h2`
-  padding-top: 2rem;
-  margin-top: 2rem;
-`
-
-const CreditsSection = styled.div`
-  width: calc(
-    ${(props) =>
-        // @ts-expect-error
-
-        props.width} - 15px
-  );
-
-  @media (max-width: 992px) {
-    width: 100%;
-  }
-`
 
 const Contributors = styled.div`
   line-height: 1.5;
@@ -63,12 +32,6 @@ const _PrincipalInvestigators = styled(Contributors)`
 
   @media (max-width: 992px) {
     columns: 1;
-  }
-`
-
-const FundingSources = styled(Contributors)`
-  li {
-    margin-bottom: 1rem;
   }
 `
 
