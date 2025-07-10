9ac55da7134dcc3e00b592a95d9cb2374875d987
Milo Hyben <milo.hyben@gmail.com>
2025-07-10 16:27:34 +1000

    Fixing Linting on team page.

--- browser/src/TeamPage/TeamPage.tsx
+++ browser/src/TeamPage/TeamPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { PageHeading } from '@gnomad/ui'
+// @ts-ignore - TS2307 Cannot fine module ... or its corresponding type declarations.
 import dataGenerationContributors from '../../about/contributors/data-generation.md'
 import DocumentTitle from '../DocumentTitle'
 import InfoPage from '../InfoPage'
