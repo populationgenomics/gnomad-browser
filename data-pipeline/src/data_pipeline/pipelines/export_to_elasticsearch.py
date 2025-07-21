ada1e18dddbb1dbd84ee45bf383a0e2d6d4912c2
Milo Hyben <milo.hyben@gmail.com>
2025-07-21 17:55:31 +1000

    Testing subset of variant table with gene_id.

--- data-pipeline/src/data_pipeline/pipelines/export_to_elasticsearch.py
+++ data-pipeline/src/data_pipeline/pipelines/export_to_elasticsearch.py
@@ -63,7 +63,29 @@ def add_xpos(ds):
 
 
 def add_variant_document_id(ds):
-    return ds.annotate(document_id=compressed_variant_id(ds.locus, ds.alleles))
+    return ds.annotate(
+        document_id=compressed_variant_id(ds.locus, ds.alleles),
+        # transcript_consequences=hl.array([ds.variant_id]), # hl.empty_set([]) # hl.array([hl.struct(gene_id='NA',)])
+        # rsids=ds.variant_id,
+        # genome=ds.variant_id,
+        # exome=ds.variant_id,
+        # coverage=ds.variant_id,
+
+        joint=ds.joint.annotate(
+            freq_comparison_stats=ds.joint.freq_comparison_stats.annotate(
+                contingency_table_test=hl.empty_set(hl.tstr),
+                # stat_union=hl.empty_set(hl.tstr)
+                ),
+            faf=hl.empty_set(hl.tstr), # | joint.faf 
+
+            # histograms=ds.variant_id,
+            # freq=ds.variant_id,
+            # fafmax=ds.variant_id,
+            # grpmax=ds.variant_id,
+            # flags=ds.variant_id,
+        ),
+        
+    )
 
 
 def truncate_clinvar_variant_ids(ds):
@@ -498,7 +520,8 @@ DATASETS_CONFIG = {
     ##############################################################################################################
     "ourdna_bioheart_variants_v4": {
         "get_table": lambda: subset_table(
-            add_variant_document_id(hl.read_table("gs://cpg-ourdna-browser-dev-test/ourDNA-browser/browser.ht"))
+            # add_variant_document_id(hl.read_table("gs://cpg-ourdna-browser-dev-test/ourDNA-browser/browser.ht"))
+            add_variant_document_id(hl.read_table("gs://cpg-ourdna-browser-dev-test/ourDNA-browser/test/annotated_browser_ht.ht"))
         ),
         "args": {
             "index": "gnomad_v4_variants",
@@ -509,7 +532,7 @@ DATASETS_CONFIG = {
                 "locus",
                 # NA in OurDNA fields:
                 # "caid",
-                # "transcript_consequences.gene_id",
+                "transcript_consequences.gene_id",
                 # "transcript_consequences.transcript_id",
                 # "vrs.alt.allele_id",
             ],
