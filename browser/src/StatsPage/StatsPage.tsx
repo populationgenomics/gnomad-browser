6a3000011ce9727c4f732671cb9b010a16fdbe8c
silkm <michael.silk@populationgenomics.org.au>
2025-08-07 11:34:10 +1000

    re-add the default gnomAD stats page to pare back

--- browser/src/StatsPage/StatsPage.tsx
+++ browser/src/StatsPage/StatsPage.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// import gnomadV4AgeDistribution from '@gnomad/dataset-metadata/datasets/gnomad-v4/ageDistribution.json'
-import { /* ExternalLink, */ PageHeading } from '@gnomad/ui'
+import gnomadV4AgeDistribution from '@gnomad/dataset-metadata/datasets/gnomad-v4/ageDistribution.json'
+import { ExternalLink, PageHeading } from '@gnomad/ui'
 
 // @ts-ignore - TS2307 Cannot fine module ... or its corresponding type declarations.
-// import BrowserPageviews from '../../about/stats/browser_pageviews.png'
+import BrowserPageviews from '../../about/stats/browser_pageviews.png'
 // @ts-ignore - TS2307 Cannot fine module ... or its corresponding type declarations.
-// import BrowserWorld from '../../about/stats/browser_world.png'
+import BrowserWorld from '../../about/stats/browser_world.png'
 // @ts-ignore - TS2307 Cannot fine module ... or its corresponding type declarations.
-// import DiversityBadge from '../../about/stats/diversity_badge.png'
+import DiversityBadge from '../../about/stats/diversity_badge.png'
 // @ts-ignore - TS2307 Cannot fine module ... or its corresponding type declarations.
-// import SnvsPerBPAvg from '../../about/stats/snvs_per_bp_avg.png'
+import SnvsPerBPAvg from '../../about/stats/snvs_per_bp_avg.png'
 
 import DocumentTitle from '../DocumentTitle'
-// import Histogram from '../Histogram'
-// import { SectionHeading } from '../help/HelpPage'
+import Histogram from '../Histogram'
+import { SectionHeading } from '../help/HelpPage'
 import InfoPage from '../InfoPage'
-// import Link from '../Link'
+import Link from '../Link'
 
-// import gnomadExomeGenomeCountsByVersion from './BarGraphData/gnomADExomeGenomeCountsByVersion.json'
-// import gnomadV4GeneticAncestryCounts from './BarGraphData/gnomadV4GeneticAncestryCounts.json'
-// import gnomadV4GeneticDiversityCounts from './BarGraphData/gnomadV4GeneticDiversityCounts.json'
-// import NumberOfVariantsInGnomadList, { SectionList } from './NumberOfVariantsInGnomadList'
-// import StackedBarGraph from './StackedBarGraph'
-// import GeneticAncestryGroupsByVersionTable from './StatsPageTables/GeneticAncestryGroupsByVersionTable'
-// import V4GeneticAncestryTable from './StatsPageTables/V4GeneticAncestryTable'
-// import StudyDiseasesInGnomadTable from './StatsPageTables/StudyDiseasesInGnomadTable'
+import gnomadExomeGenomeCountsByVersion from './BarGraphData/gnomADExomeGenomeCountsByVersion.json'
+import gnomadV4GeneticAncestryCounts from './BarGraphData/gnomadV4GeneticAncestryCounts.json'
+import gnomadV4GeneticDiversityCounts from './BarGraphData/gnomadV4GeneticDiversityCounts.json'
+import NumberOfVariantsInGnomadList, { SectionList } from './NumberOfVariantsInGnomadList'
+import StackedBarGraph from './StackedBarGraph'
+import GeneticAncestryGroupsByVersionTable from './StatsPageTables/GeneticAncestryGroupsByVersionTable'
+import V4GeneticAncestryTable from './StatsPageTables/V4GeneticAncestryTable'
+import StudyDiseasesInGnomadTable from './StatsPageTables/StudyDiseasesInGnomadTable'
 
-// import {
-//   InferredSexAllV4Table,
-//   InferredSexNonUKBV4Table,
-// } from './StatsPageTables/InferredSexPerGeneticAncestryTables'
+import {
+  InferredSexAllV4Table,
+  InferredSexNonUKBV4Table,
+} from './StatsPageTables/InferredSexPerGeneticAncestryTables'
 
-const _TwoColumnLayout = styled.div`
+const TwoColumnLayout = styled.div`
   display: flex;
   justify-content: space-around;
 
@@ -42,7 +42,7 @@ const _TwoColumnLayout = styled.div`
   }
 `
 
-const _ResponsiveHalfWidthColumn = styled.div`
+const ResponsiveHalfWidthColumn = styled.div`
   width: 50%;
 
   @media (max-width: 992px) {
@@ -50,7 +50,7 @@ const _ResponsiveHalfWidthColumn = styled.div`
   }
 `
 
-const _ResponsiveGnomadSamplesContainer = styled.div`
+const ResponsiveGnomadSamplesContainer = styled.div`
   width: 70%;
 
   @media (max-width: 992px) {
@@ -58,7 +58,7 @@ const _ResponsiveGnomadSamplesContainer = styled.div`
   }
 `
 
-const _DiversityBarGraphContainer = styled.div`
+const DiversityBarGraphContainer = styled.div`
   display: flex;
   justify-content: space-around;
 
@@ -68,7 +68,7 @@ const _DiversityBarGraphContainer = styled.div`
   }
 `
 
-const _DiversityBarGraph = styled.div`
+const DiversityBarGraph = styled.div`
   width: 70%;
 
   @media (max-width: 992px) {
@@ -77,7 +77,7 @@ const _DiversityBarGraph = styled.div`
   }
 `
 
-const _SexDistributionList = styled.div`
+const SexDistributionList = styled.div`
   width: 30%;
 
   @media (max-width: 992px) {
@@ -85,12 +85,12 @@ const _SexDistributionList = styled.div`
   }
 `
 
-const _CenteredContainer = styled.div`
+const CenteredContainer = styled.div`
   display: flex;
   justify-content: space-around;
 `
 
-const _ResponsiveTable = styled.div`
+const ResponsiveTable = styled.div`
   display: flex;
   justify-content: space-around;
 
@@ -99,11 +99,11 @@ const _ResponsiveTable = styled.div`
   }
 `
 
-const _StatsSection = styled.div`
+const StatsSection = styled.div`
   margin-bottom: 5em;
 `
 
-const _StatsHighlightColorBlock = styled.div`
+const StatsHighlightColorBlock = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -116,22 +116,22 @@ const _StatsHighlightColorBlock = styled.div`
   text-align: center;
 `
 
-const _StatsHighlightTitle = styled.h1`
+const StatsHighlightTitle = styled.h1`
   margin: 0;
   font-size: 3.75em;
 `
 
-const _StatsHighlightText = styled.p`
+const StatsHighlightText = styled.p`
   margin: 0;
   font-size: 1.25em;
 `
 
-const _CountriesColoredText = styled.span`
+const CountriesColoredText = styled.span`
   color: #508a14;
   font-weight: bold;
 `
 
-const _DiversityBarGraphTooltip = (row: any) => {
+const DiversityBarGraphTooltip = (row: any) => {
   return (
     <>
       <b>{row.label}</b>
@@ -149,7 +149,6 @@ const _DiversityBarGraphTooltip = (row: any) => {
   )
 }
 
-/*
 const StatsHighlightBlock = ({
   title,
   text,
@@ -168,30 +167,26 @@ const StatsHighlightBlock = ({
     </StatsHighlightColorBlock>
   )
 }
-*/
 
-// const gnomadBlue = '#0E6FBF'
-// const gnomadGreen = '#508A14'
+const gnomadBlue = '#0E6FBF'
+const gnomadGreen = '#508A14'
 
-// const barGraphTooltip = (row: any) => (
-//   <>
-//     <b>{row.label}</b>
-//     <div>{row.Exomes.toLocaleString()} exomes</div>
-//     <div>{row.Genomes.toLocaleString()} genomes</div>
-//   </>
-// )
+const barGraphTooltip = (row: any) => (
+  <>
+    <b>{row.label}</b>
+    <div>{row.Exomes.toLocaleString()} exomes</div>
+    <div>{row.Genomes.toLocaleString()} genomes</div>
+  </>
+)
 
 const StatsPage = () => {
   return (
     <InfoPage>
       <DocumentTitle title="Stats" />
       {/* @ts-expect-error */}
-      <PageHeading id="gnomad-stats">What&apos;s in OurDNA Browser</PageHeading>
+      <PageHeading id="gnomad-stats">What&apos;s in gnomAD</PageHeading>
       <div>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-       {/* <StatsSection style={{ marginTop: '2em' }}>
+        <StatsSection style={{ marginTop: '2em' }}>
           <TwoColumnLayout>
             <div>
               <h2>gnomAD v4 includes 807,162 individuals</h2>
@@ -232,9 +227,9 @@ const StatsPage = () => {
               </CenteredContainer>
             </ResponsiveHalfWidthColumn>
           </TwoColumnLayout>
-        </StatsSection> */}
+        </StatsSection>
 
-       {/* <StatsSection>
+        <StatsSection>
           <SectionHeading id="age-and-sex-distribution">
             What is the age and sex distribution in gnomAD?
           </SectionHeading>
@@ -288,9 +283,9 @@ const StatsPage = () => {
             To learn more about how we calculate the sex and age distribution please see our{' '}
             <Link to="/help">FAQs</Link>
           </p>
-        </StatsSection> */}
+        </StatsSection>
 
-       {/* <StatsSection>
+        <StatsSection>
           <SectionHeading id="samples">Where do gnomAD samples come from?</SectionHeading>
           <div style={{ width: '100%' }}>
             <TwoColumnLayout>
@@ -320,10 +315,10 @@ const StatsPage = () => {
             To see a list of studies included in gnomAD and data contributors please visit our{' '}
             <Link to="/about">about page</Link>.
           </p>
-        </StatsSection> */}
+        </StatsSection>
 
-     {/*   <StatsSection>
-          <SectionHeading id="diversity">Diversity in OurDNA Browser</SectionHeading>
+        <StatsSection>
+          <SectionHeading id="diversity">Diversity in gnomAD</SectionHeading>
 
           <h3 style={{ marginBottom: '2em' }}>Genetic ancestry groups in gnomAD by version</h3>
 
@@ -342,6 +337,7 @@ const StatsPage = () => {
               <p>
                 To learn more about how we determine genetic ancestry groups please see our{' '}
                 <Link to="help/ancestry">help page</Link> and{' '}
+                {/* @ts-expect-error TS(2786) FIXME: 'ExternalLink' cannot be used as a JSX component. */}
                 <ExternalLink href="https://gnomad.broadinstitute.org/news/2023-11-genetic-ancestry">
                   blog post
                 </ExternalLink>{' '}
@@ -397,9 +393,9 @@ const StatsPage = () => {
           <ResponsiveTable style={{ marginBottom: '3em' }}>
             <InferredSexNonUKBV4Table />
           </ResponsiveTable>
-        </StatsSection> */}
+        </StatsSection>
 
-     {/*   <StatsSection>
+        <StatsSection>
           <SectionHeading id="study-provided-labels">
             Study-provided labels and genetic ancestry groups
           </SectionHeading>
@@ -420,9 +416,9 @@ const StatsPage = () => {
           <ResponsiveTable>
             <V4GeneticAncestryTable />
           </ResponsiveTable>
-        </StatsSection> */}
+        </StatsSection>
 
-      {/*  <StatsSection>
+        <StatsSection>
           <SectionHeading id="study-diseases">Study Diseases in gnomAD</SectionHeading>
 
           <p style={{ marginBottom: '2em' }}>
@@ -437,9 +433,9 @@ const StatsPage = () => {
           <ResponsiveTable style={{ marginBottom: '3em' }}>
             <StudyDiseasesInGnomadTable />
           </ResponsiveTable>
-        </StatsSection> */}
+        </StatsSection>
 
-       {/* <StatsSection>
+        <StatsSection>
           <SectionHeading id="browser">gnomAD Browser Stats</SectionHeading>
           <p>{`The gnomAD browser averages ~200,000 page views per week and had >377,000 unique users in the last year`}</p>
           <TwoColumnLayout>
@@ -450,7 +446,7 @@ const StatsPage = () => {
               <img alt="Browser users location" src={BrowserWorld} width="100%" />
             </ResponsiveHalfWidthColumn>
           </TwoColumnLayout>
-        </StatsSection> */}
+        </StatsSection>
       </div>
     </InfoPage>
   )
