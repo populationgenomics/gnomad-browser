afae87578e625b73800e1b2a68e1acb6877d778a
Milo Hyben <milo-hyben@users.noreply.github.com>
2025-08-11 11:18:48 +1000

    Text & tooltips updates (#62)
    * Update package meta data.
    
    * Fix no variants found message.
    
    * Add reference to gnomAD variants.

--- browser/src/VariantList/Variants.tsx
+++ browser/src/VariantList/Variants.tsx
@@ -268,7 +268,7 @@ const Variants = ({
     return (
       <TrackPageSection>
         <h2 style={{ margin: '2em 0 0.25em' }}>OurDNA variants</h2>
-        <p>No gnomAD variants found.</p>
+        <p>No matching variants found.</p>
       </TrackPageSection>
     )
   }
