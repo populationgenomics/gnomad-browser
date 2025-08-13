3027fa735354a7d637fe59a67bc2ef49bcc1d2cb
silkm <michael.silk@populationgenomics.org.au>
2025-08-13 11:26:35 +1000

    fix lint error

--- browser/src/Legend.tsx
+++ browser/src/Legend.tsx
@@ -12,8 +12,8 @@ const LegendWrapper = styled.ul`
 
 const LegendItem = styled.li`
   display: flex;
-  align-items: center;
   flex-shrink: 0;
+  align-items: center;
   margin: 0 1em 0.33em 0;
 `
 
