f02fe7c0b345c08cb9c552b6387ff943fef47e80
Milo Hyben <milo.hyben@gmail.com>
2025-11-20 11:38:02 +1100

    Fix for cluster name, can't be used in kubectl.

--- deploy/deployctl/subcommands/elasticsearch.py
+++ deploy/deployctl/subcommands/elasticsearch.py
@@ -6,15 +6,15 @@ import typing
 from deployctl.shell import kubectl
 
 
-def get_elasticsearch_password(cluster_name: str, namespace: str) -> None:
+def get_elasticsearch_password(_cluster_name: str, namespace: str) -> None:
     # ECK creates this secret when the cluster is created.
     print(
         kubectl(
             [
-                f"--cluster {cluster_name}",
                 f"-n={namespace}",
                 "get",
                 "secret",
+                # gnomad is hardcoded as clustername in terraforms
                 "gnomad-es-elastic-user",
                 "-o=go-template={{.data.elastic | base64decode}}",
             ]
@@ -22,14 +22,14 @@ def get_elasticsearch_password(cluster_name: str, namespace: str) -> None:
     )
 
 
-def load_datasets(cluster_name: str, namespace: str, dataproc_cluster: str, secret: str, datasets: str):
+def load_datasets(_cluster_name: str, namespace: str, dataproc_cluster: str, secret: str, datasets: str):
     # Matches service name in deploy/manifests/elasticsearch.load-balancer.yaml.jinja2
     elasticsearch_load_balancer_ip = kubectl(
         [
-            f"--cluster {cluster_name}",
             f"-n={namespace}",
             "get",
             "service",
+            # gnomad is hardcoded as clustername in terraforms
             "gnomad-elasticsearch-lb",
             "--output=jsonpath={.status.loadBalancer.ingress[0].ip}",
         ]
