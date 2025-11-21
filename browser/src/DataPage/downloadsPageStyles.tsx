24e2297058d4a65bfc2d21094542aae27d3a0a1b
Amy Miniter <40307120+amyminiter@users.noreply.github.com>
2025-11-21 13:57:27 +1100

    Remove period punctuation from download links

--- browser/src/DataPage/downloadsPageStyles.tsx
+++ browser/src/DataPage/downloadsPageStyles.tsx
@@ -157,7 +157,7 @@ export const GetUrlButtons = ({
             key="gcp"
             aria-label={`VCF ${label}`}
             label={label}
-            url={`gs://${gcsBucket}${path} .`}
+            url={`gs://${gcsBucket}${path}`}
             logClicks={logClicks}
           >
             Get VCF
@@ -176,7 +176,7 @@ export const GetUrlButtons = ({
                   key="gcp"
                   aria-label={`VCF CSI path ${label}`}
                   label={label}
-                  url={`gs://${gcsBucket}${path}.${associatedFileType.toLowerCase()} .`}
+                  url={`gs://${gcsBucket}${path}.${associatedFileType.toLowerCase()}`}
                   logClicks={logClicks}
                 >
                   Get VCF CSI
